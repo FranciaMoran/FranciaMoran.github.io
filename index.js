e793ce0f52c4e3da7641d17d25283acbf34d2f82
Francia Moran <franciam115@gmail.com>
2018-10-05 14:40:16 -0500

    js file added 2

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 'use strict'
 
 let musicImages = [
-  `./images/createaccountmusic.jpg`,
-  `./images/reactcapstone2.jpg`,
-  `./images/reactcapstone3.jpg`,
-  `./images/searchmusic.jpg`
+  `./images/createaccountmusic.JPG`,
+  `./images/reactcapstone2.JPG`,
+  `./images/reactcapstone3.JPG`,
+  `./images/searchmusic.JPG`
 ]
 
 let calorieImages = [
-  `./images/Capture2.jpg`,
-  `./images/nutritionapi.jpg`,
-  `./images/totals.jpg`,
-  `./images/loggedcalorie.jpg`,
-  `./images/custom.jpg`
+  `./images/Capture2.JPG`,
+  `./images/nutritionapi.JPG`,
+  `./images/totals.JPG`,
+  `./images/loggedcalorie.JPG`,
+  `./images/custom.JPG`
 ]
 
 function arrows() {
